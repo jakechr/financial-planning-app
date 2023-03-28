a92ce88ed098bbf5aa26c19d4fa5817b60f766d4
adammryan <adammryan17@gmail.com>
2023-03-28 15:46:55 -0600

    add-cypress

---
+++ frontend/src/App.cy.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from './App'
+
+describe('<App />', () => {
+    it('renders', () => {
+        cy.mount(<App />)
+    })
+
+    it('test text', () => {
+        cy.mount(<App/>)
+        cy.get('h1').should('have.text', 'Hello World!')
+    })
+})
