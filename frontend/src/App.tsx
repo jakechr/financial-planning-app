2586bcf0d6ede79a3176c05ef7e2cc2c7622200f
adammryan <adammryan17@gmail.com>
2023-03-30 13:43:44 -0600

    basic cypress tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import styles from './App.less';
 
 function App() {
+
+    const [counter, setCounter] = React.useState(0);
+    const onButtonClick = () => {
+        setCounter(counter + 1);
+    }
+
 	return (
 		<div className={styles.app}>
 			<h1 className={styles.appText}>Hello World!</h1>
+            <div className={styles.testBlock}>
+                <input type="text" data-cy="input" />
+                <div data-cy="counter">{counter}</div>
+                <button data-cy="button" onClick={onButtonClick}>Increment Counter</button>
+            </div>
 		</div>
 	);
 }
--- frontend/src/cypressTests/App.cy.jsx
+++ frontend/src/cypressTests/App.cy.jsx
@@ -10,4 +10,28 @@ describe('<App />', () => {
         cy.mount(<App/>)
         cy.get('h1').should('have.text', 'Hello World!')
     })
+
+    it('test input', () => {
+        const INPUT_TEXT = "This is a test";
+        cy.mount(<App />)
+        const input = cy.get('[data-cy="input"]')
+        input.should('have.text', '')
+        
+        input.type(INPUT_TEXT)
+        input.should('have.value', INPUT_TEXT)
+    })
+    
+    it('test counter', () => {
+        cy.mount(<App />)
+
+        cy.get('[data-cy="counter"]').as('counter')
+        cy.get('[data-cy="button"]').as('button')
+        cy.get('@counter').should('have.text', '0')
+
+        const numClicks = 20;
+        for (let i = 0; i < numClicks; i++) {
+            cy.get('@button').click()
+        }
+        cy.get('@counter').should('have.text', numClicks)
+    })
 })
