6cbf6e47255805a72cf18b727929ebe59e431e0e
adammryan <adammryan17@gmail.com>
2023-04-12 13:58:32 -0600

    Add Goal To Goals Table

--- frontend/src/app/views/dashboard/Goals.jsx
+++ frontend/src/app/views/dashboard/Goals.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import ReusableCanvas from './ReusableCanvas';
 import ReusableCanvasContent from './ReusableCanvasContent';
 import GoalRow, { GoalHeader } from './tablerows/GoalRow';
+
+import Dialog from '@mui/material/Dialog';
+import TextField from '@mui/material/TextField';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import Button from '@mui/material/Button';
 
 export default function GoalsDashboard() {
   const canvasName = 'Goals';
   const canvasDescription = 'Track your spending goals here.';
   const canvasButton = 'Add New Goal';
 
-  const mockGoals = [
+  
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const handleClickOpen = () => setDialogOpen(true);
+  const handleClose = () => setDialogOpen(false);
+
+  const onAddButtonPress = () => {
+    handleClickOpen();
+  }
+
+  let mockGoals = [
     {
       name: "Subaru",
       date: "10/1/2024",
@@ -21,21 +39,114 @@ export default function GoalsDashboard() {
       description: "get a nicer car"
     },
   ];
-  const tableRows = mockGoals.map((goal, index) => (
+
+  const [goalsToShow, setGoalsToShow] = useState(mockGoals)
+
+  const tableRows = goalsToShow.map((goal, index) => (
     <GoalRow key={index} goal={goal} />
   ));
 
   const canvasContent = <ReusableCanvasContent
     addButtonTitle={canvasButton}
+    onAddButtonPress={onAddButtonPress}
     tableHeader={<GoalHeader/>}
     tableRows={tableRows}
   />;
 
+  const [newGoalName, setNewGoalName] = useState('');
+  const [newGoalDescription, setNewGoalDescription] = useState('');
+  const [newGoalDate, setNewGoalDate] = useState('');
+  const [newGoalAmount, setNewGoalAmount] = useState('');
+
+  const addGoalFields = [
+    {
+        id: 'name',
+        type: 'text',
+        label: 'Name',
+        setter: setNewGoalName
+    },
+    {
+        id: 'description',
+        type: 'text',
+        label: 'Description',
+        setter: setNewGoalDescription,
+    },
+    {
+        id: 'date',
+        type: 'date',
+        label: '',
+        setter: setNewGoalDate,
+    },
+    {
+        id: 'amount',
+        type: 'number',
+        label: '$ Amount',
+        setter: setNewGoalAmount,
+    }
+  ]
+
+  const handleSubmit = () => {
+      const newGoal = {
+        name: newGoalName,
+        description: newGoalDescription,
+        date: newGoalDate,
+        amount: parseInt(newGoalAmount)
+      }
+
+      // Hit the server, on success add goal
+      
+      mockGoals.push(newGoal)
+      setGoalsToShow(mockGoals)
+
+      setDialogOpen(false)
+  }
+
   return (
-    <ReusableCanvas
-      canvasName={canvasName}
-      canvasDescription={canvasDescription}
-      canvasContent={canvasContent}
-    />
+    <>
+        <ReusableCanvas
+            canvasName={canvasName}
+            canvasDescription={canvasDescription}
+            canvasContent={canvasContent}
+        />
+        <AddGoalDialog isOpen={isDialogOpen} handleClose={handleClose} handleSubmit={handleSubmit} fields={addGoalFields}/>
+    </>
   );
 };
+
+
+const AddGoalDialog = ({isOpen, fields, handleClose, handleSubmit}) => {
+    return (
+        <Dialog open={isOpen} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">New Goal</DialogTitle>
+
+            <DialogContent>
+                <DialogContentText>
+                    Add your new goal, completion date, and amount you want to save up.
+                </DialogContentText>
+
+                {fields.map((field, i) => (
+                    <TextField
+                        key={i}
+                        fullWidth
+                        autoFocus={i === 0}
+                        name={field.name}
+                        type={field.type}
+                        label={field.label}
+                        margin="dense"
+                        onChange={(event) => field.setter(event.target.value)}
+                    />)
+                )}
+            </DialogContent>
+
+            <DialogActions>
+                <Button variant="outlined" color="secondary" onClick={handleClose}>
+                    Cancel
+                </Button>
+
+                <Button onClick={handleSubmit} color="primary">
+                    Add New
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
